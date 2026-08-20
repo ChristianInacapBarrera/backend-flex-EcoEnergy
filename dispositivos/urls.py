56ef9d252d6afe165a2e9190935985623e7b14f8
Christian Barrera <christianbarrera@MacBook-Air-de-Christian.local>
2026-08-19 21:42:41 -0400

    Se inicia aplicacion, ase agregan dispositivos, se modifica urls dispositivos y urls config, se modifica views

---
+++ dispositivos/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+app_name = "dispositivos"
+urlpatterns = [
+    path("", views.inicio, name="inicio"),
+    path("zonas/<int:zona_id>/dispositivos/", views.dispositivos_zona, name="por_zona")    
+    ]
+
