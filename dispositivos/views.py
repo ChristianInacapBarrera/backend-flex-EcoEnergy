62a073376f116a76dbc5a9c6a394e5c70140d1ca
Christian Barrera <christianbarrera@MacBook-Air-de-Christian.local>
2026-08-20 20:24:00 -0400

    Se modifica dispositivos_id para que funcione con render en vez de HttpResponse

--- dispositivos/views.py
+++ dispositivos/views.py
@@ -3,14 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
-def inicio(request):
+"""def inicio(request): #Forma basica de mostrar contenido
     return HttpResponse(
         "<h1>EcoEnergy</h1>"
         "<p>Back End en funcionamiento</p>"
-        )
+        )"""
     
+#Esats funciones dirigen que mostrar
+
 # dispositivos/views.py 
-def dispositivos_zona(request, zona_id): 
+def dispositivos_zona(request, zona_id): #clase 3
     if zona_id != 3:
         return HttpResponse(
             "Zona no encontrada", status=404
@@ -19,13 +21,17 @@ def dispositivos_zona(request, zona_id):
         f"Dispositivos de la zona {zona_id}"
     )
     
-def dispositivos_id(request, dispositivo_id):
-    if dispositivo_id != 10:
-        return HttpResponse(
-            "Dispositivo inexistente", status=404
-        )
-    return HttpResponse(
-        f"Dispositivo {dispositivo_id} encontrado."
+def dispositivos_id(request, dispositivo_id): #implementacion autonoma
+    if dispositivo_id == 10:
+        context2 = {"mensaje": f"Dispositivo {dispositivo_id} inexistente"}
+    else:
+        context2= {"mensaje": f"Dispositivo {dispositivo_id} encontrado."}
+    
+    
+    return render(
+        request,
+        "dispositivos/dispositivo.html",
+        context2,
     )
     
 def inicio(request):
