71ee016704792997cc6a7d0e6efcb62613556aa3
Christian Barrera <christianbarrera@MacBook-Air-de-Christian.local>
2026-08-26 22:24:41 -0400

    Se completa la clase 5

--- dispositivos/views.py
+++ dispositivos/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .services import cargar_dispositivos
 # Create your views here.
 
 from django.http import HttpResponse
@@ -57,3 +57,34 @@ def catalogo(request):
         "dispositivos/catalogo.html",
         {"dispositivos": dispositivos},
     )
+    
+
+def catalogo(request):
+    dispositivos = cargar_dispositivos()
+    
+    activos = sum(
+        1 for item in dispositivos
+        if item["estado"] == "Activo"
+    )
+    
+    contexto = {
+        "dispositivos": dispositivos,
+        "total": len(dispositivos),
+        "total_activos": activos,
+    }
+    
+    return render(
+        request, "dispositivos/catalogo.html", contexto
+    )
+    
+
+
+
+
+
+
+
+
+
+
+
